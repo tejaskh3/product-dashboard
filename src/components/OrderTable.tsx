a09450118a67b2a6bfbbb9453368b957429fa033
tejaskh3 <tejaskh3@gmail.com>
2024-06-20 01:31:07 +0630

    fix: remove unwanted imports

--- src/components/OrderTable.tsx
+++ src/components/OrderTable.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Order } from "../types/ordersData";
 import OrderItem from "./OrderItem";
 import Pagination from "./Pagination";
