8db153849eff79ba87b9f933bc65afe74f45b604
tejaskh3 <tejaskh3@gmail.com>
2024-06-19 16:34:27 +0530

    refactor:add login and logout button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,16 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import LoginButton from "./components/LoginButton";
+import LogoutButton from "./components/LogoutButton";
 
-const LoginButton = () => {
-  const { loginWithRedirect } = useAuth0();
-
-  return <button onClick={() => loginWithRedirect()}>Log In</button>;
-};
-const LogoutButton = () => {
-  const { logout } = useAuth0();
-
-  return (
-    <button
-      onClick={() =>
-        logout({ logoutParams: { returnTo: window.location.origin } })
-      }
-    >
-      Log Out
-    </button>
-  );
-};
 const App: React.FC = () => {
   const { user } = useAuth0();
   console.log(user);
   return (
     <div>
-      {user ? <LogoutButton></LogoutButton> : <LoginButton></LoginButton>}
+      <h1 className="text-3xl font-bold underline">
+        {user ? <LogoutButton></LogoutButton> : <LoginButton></LoginButton>}
+      </h1>
     </div>
   );
 };
