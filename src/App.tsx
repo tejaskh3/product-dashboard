0632f6cf94d255b158a0b0a829866fc85655886d
tejaskh3 <tejaskh3@gmail.com>
2024-06-19 14:43:29 +0530

    feat:rough implementaion of login and logout with google

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const LoginButton = () => {
+  const { loginWithRedirect } = useAuth0();
+
+  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+};
+const LogoutButton = () => {
+  const { logout } = useAuth0();
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+    <button
+      onClick={() =>
+        logout({ logoutParams: { returnTo: window.location.origin } })
+      }
+    >
+      Log Out
+    </button>
+  );
+};
+const App: React.FC = () => {
+  const { user } = useAuth0();
+  console.log(user);
+  return (
+    <div>
+      {user ? <LogoutButton></LogoutButton> : <LoginButton></LoginButton>}
+    </div>
+  );
+};
+export default App;
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,6 +2,7 @@
 
 interface ImportMetaEnv {
   readonly VITE_AUTH_CLIENT_ID: string;
+  readonly VITE_DOMAIN: string;
   // more env variables...
 }
 
